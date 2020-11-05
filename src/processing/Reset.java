a1e4009b8c0b580d4499c26b629466b98ce7227d
satchit24 <48960354+satchit24@users.noreply.github.com>
2020-11-05 18:13:55 +0530

    Class Reset Documentation

--- src/processing/Reset.java
+++ src/processing/Reset.java
@@ -4,45 +4,51 @@ import java.util.ArrayList;
 
 import processing.boardobject.BoardObject;
 import processing.utility.*;
-/*
+/**
+ * Reset Class clears the screen
+ * ables undo and redo
+ * but not select delete and rotate
+ * 
  * @author Satchit Desai
  * @reviewer Himanshu Jain
  */
 public class Reset {
 	
+	/*
+	 *  The function makes a white filled rectangle object
+	 * covers the whole screen with undo enabled
+	 */
 	public static BoardObject screenReset(ObjectId newObjId, Timestamp newTimestamp, UserId newUserId, boolean reset) {
 		
+		/* Get BoardDimensions from UI */
 		Dimension dimension = ClientBoardState.boardDimension;
 		
 		int rows = dimension.numRows;
 		
 		int cols = dimension.numCols;
 		
+		Intensity whiteIntensity = new Intensity(255,255,255);
+		
 		ArrayList<Pixel> pixels = new ArrayList<Pixel>();
 		
+		/* Traverse through screen and make intensity white */
 		for(int i = 0; i < rows; i++) {
 			
 			for(int j = 0; j < cols; j++) {
 				
-				Intensity whiteIntensity = new Intensity(255,255,255);
-				
 				Position whitePosition = new Position(i,j);
 				
-				Pixel whitePixel = new Pixel(whitePosition,
-											whiteIntensity);
+				Pixel whitePixel = new Pixel(whitePosition, whiteIntensity);
 				
 				pixels.add(whitePixel);
 				
 			}
 		}
 		
-		BoardObject resetObj = new BoardObject(	pixels,
-												newObjId,
-												newTimestamp,
-												newUserId,
-												reset
-												);
-		
+		/* Creates Object of the white rectangle acting as clear screen mask */
+		BoardObject resetObj = 
+				new BoardObject(pixels, newObjId, newTimestamp, newUserId, reset);
+
 		return resetObj;
 	}
 }
--- src/processing/handlers/PortHandler.java
+++ src/processing/handlers/PortHandler.java
@@ -3,10 +3,6 @@ package processing.handlers;
 import networking.INotificationHandler;
 import processing.ClientBoardState;
 import processing.utility.*;
-
-<<<<<<< HEAD
-public class PortHandler implements INotificationHandler {
-=======
 /**
  * This class handles Port number of the Board Server received from the server
  * 
@@ -16,7 +12,6 @@ public class PortHandler implements INotificationHandler {
  */
 
 public class PortHandler implements INotificationHandler{
->>>>>>> ca2577c6c9ae0841541caed5b84aecd066607141
 	
 		public void onMessageReceived(String message) {
 			Port portNumber = new Port(Integer.parseInt(message));
