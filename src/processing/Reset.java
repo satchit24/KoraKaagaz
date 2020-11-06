69539cb8d033b8f5757dfa2e2cc1590782edc529
satchit24 <48960354+satchit24@users.noreply.github.com>
2020-11-06 23:46:30 +0530

    Revise Reset

--- src/processing/Reset.java
+++ src/processing/Reset.java
@@ -17,8 +17,11 @@ public class Reset {
 	/*
 	 *  The function makes a white filled rectangle object
 	 * covers the whole screen with undo enabled
+	 * 
+	 * @param Reset flag so reset object is not selected
+	 * deleted rotated color changed
 	 */
-	public static BoardObject screenReset(ObjectId newObjId, Timestamp newTimestamp, UserId newUserId, boolean reset) {
+	public static void screenReset(UserId newUserId,boolean reset) {
 		
 		/* Get BoardDimensions from UI */
 		Dimension dimension = ClientBoardState.boardDimension;
@@ -31,7 +34,9 @@ public class Reset {
 		
 		ArrayList<Pixel> pixels = new ArrayList<Pixel>();
 		
-		/* Traverse through screen and make intensity white */
+		/* Traverse through screen and assign position
+		 * and white intensity to each pixel
+		 */
 		for(int i = 0; i < rows; i++) {
 			
 			for(int j = 0; j < cols; j++) {
@@ -45,10 +50,24 @@ public class Reset {
 			}
 		}
 		
+		Timestamp newTimestamp = Timestamp.getCurrentTime();
+		
+		ObjectId newObjId = new ObjectId(newUserId, newTimestamp);
+		
 		/* Creates Object of the white rectangle acting as clear screen mask */
 		BoardObject resetObj = 
 				new BoardObject(pixels, newObjId, newTimestamp, newUserId, reset);
-
-		return resetObj;
+		
+		//Insert BoardObject in the Map
+		ClientBoardState.maps.insertObjectIntoMaps(resetObj);
+				
+		//Push BoardObject in undo stack
+		stackUtil(resetObj);
+	}
+	
+	private static void stackUtil(BoardObject newObj) {
+		
+		UndoRedo.pushIntoStack(newObj);
+	
 	}
 }
